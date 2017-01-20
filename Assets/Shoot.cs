72b375b2eab5d0cfb4221dfec2df69e1d8ea68a3
oliegjio <oliegjio@gmail.com>
2017-01-20 18:11:02 +0300

    Added Shooting With Raycast

---
+++ Assets/Shoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public float fireRate = 0.5f;
+    float lastShoot = 0;
+    RaycastHit hit;
+    float range = 300;
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        SetInitialReferences();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckForInput();
+	}
+
+    void SetInitialReferences()
+    {
+        myTransform = transform;
+    }
+
+    void CheckForInput()
+    {
+        if(Input.GetKey(KeyCode.Mouse0) && lastShoot + fireRate < Time.time)
+        {
+            lastShoot = Time.time;
+            Debug.DrawRay(myTransform.position, myTransform.forward, Color.red, 3);
+            if(Physics.Raycast(myTransform.position, myTransform.forward, out hit, range) && hit.transform.CompareTag("Enemy"))
+            {
+                Debug.Log("Hit: " + hit.transform.name);
+            }
+        }
+    }
+}
---
+++ Assets/Welcome.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SimpleShutter
+{
+    public class Welcome : MonoBehaviour
+    {
+
+        public string myWelcomeMessage = "Welcome Message";
+        public Text textWelcome;
+        public Text textVersion;
+        public float canvasDisableTime = 1;
+        public GameObject canvasWelcome;
+
+        void Start()
+        {
+            SetInitialReferences();
+            PrintWelcomeMessage();
+            Invoke("DisableCanvas", canvasDisableTime);
+            StartCoroutine(DisableTextVersion(canvasDisableTime));
+        }
+
+        void SetInitialReferences()
+        {
+            textWelcome = GameObject.Find("TextWelcome").GetComponent<Text>();
+            textVersion = GameObject.Find("TextVersion").GetComponent<Text>();
+            canvasWelcome = GameObject.Find("CanvasWelcome");
+        }
+
+        void PrintWelcomeMessage()
+        {
+            if (textWelcome)
+            {
+                textWelcome.text = myWelcomeMessage;
+            }
+            else
+            {
+                Debug.LogWarning("textWelcome not assigned!");
+            }
+        }
+
+        void DisableCanvas()
+        {
+            canvasWelcome.SetActive(false);
+        }
+
+        IEnumerator DisableTextVersion(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            if (textVersion)
+            {
+                textVersion.text = "";
+            }
+        }
+    }
+}
+
