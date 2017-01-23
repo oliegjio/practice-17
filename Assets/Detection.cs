3e90321ed146b35827438f556b36472213ce9d5f
oliegjio <oliegjio@gmail.com>
2017-01-23 11:14:54 +0300

    Layers & Raycast

---
+++ Assets/Detection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleShutter
+{
+    public class Detection : MonoBehaviour
+    {
+
+        RaycastHit hit;
+        [SerializeField] private LayerMask detectionLayer;
+        float checkRate = 0.5f;
+        float nextCheck;
+        float range = 50;
+        Transform myTransform;
+
+        void Start()
+        {
+            SetInitialReferences();
+        }
+
+        void Update()
+        {
+            DetectItems();
+        }
+
+        void SetInitialReferences()
+        {
+            detectionLayer = 1 << 9;
+            myTransform = transform;
+        }
+
+        void DetectItems()
+        {
+            if(Time.time > nextCheck)
+            {
+                nextCheck = Time.time + checkRate;
+                if(Physics.Raycast(myTransform.position, myTransform.forward, out hit, range, detectionLayer))
+                {
+                    Debug.Log(hit.transform.name + " is an " + detectionLayer.value);
+                }
+            }
+        }
+    }
+}
