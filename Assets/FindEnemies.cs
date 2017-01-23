73e20f9113c0271202fb5789818beaec937ebbeb
oliegjio <oliegjio@gmail.com>
2017-01-23 07:51:20 +0300

    Finding Objects With Tags

---
+++ Assets/FindEnemies.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleShutter
+{
+    public class FindEnemies : MonoBehaviour
+    {
+        GameObject[] enemies;
+
+        // Use this for initialization
+        void Start()
+        {
+            SearchForEnemies();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void SearchForEnemies()
+        {
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            if(enemies.Length > 0)
+            {
+                foreach(GameObject enemy in enemies)
+                {
+                    enemy.SetActive(false);
+                }
+            }
+        }
+    }
+}
