342bfa76b5814ea15e3eb73183b3a5f82d09831d
oliegjio <oliegjio@gmail.com>
2017-01-24 09:27:58 +0300

    WalkThroughWall, TriggerExample etc.

---
+++ Assets/TriggerExample.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleShutter
+{
+    public class TriggerExample : MonoBehaviour
+    {
+        private WalkThroughWall walkThroughWallScript;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            walkThroughWallScript.SetLayerToNotSolid();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            walkThroughWallScript.SetLayerToDefault();
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            Debug.Log("Object " + other.name + " stay on the trigger!");
+        }
+
+        private void SetInitialReferences()
+        {
+            if(GameObject.Find("TheWall"))
+            {
+                walkThroughWallScript = GameObject.Find("TheWall").GetComponent<WalkThroughWall>();
+            }
+        }
+
+        void Start()
+        {
+            SetInitialReferences();
+        }
+    }
+}
---
+++ Assets/WalkThroughWall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleShutter
+{
+    public class WalkThroughWall : MonoBehaviour
+    {
+        private Color myColor = new Color(255, 0, 0, 0.5f);
+
+        //private void OnEnable()
+        //{
+        //    gameObject.layer = LayerMask.NameToLayer("Not Solid");
+        //}
+
+        //private void OnDisable()
+        //{
+        //    gameObject.layer = LayerMask.NameToLayer("Default");
+        //}
+
+        public void SetLayerToNotSolid()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Not Solid");
+            GetComponent<Renderer>().material.color = myColor;
+        }
+
+        public void SetLayerToDefault()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+
+    }
+}
